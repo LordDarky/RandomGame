1ef38dc60b5add31f0146500190ee1f256367c3b
BuildTools <unconfigured@null.spigotmc.org>
2016-12-28 17:10:59 +0100

    Added The Actual program

---
+++ randomgame.py
@@ -0,0 +1,14 @@
+import os
+import random
+import string
+import time
+text_file = open("games.txt", "r")
+games = text_file.read().split(',')
+print ("Loading games")
+for items in games:
+    print(items)
+print ("Selected game:")
+selectedgame = random.choice(games)
+print(selectedgame)
+os.startfile("%s" % selectedgame)
+time.sleep(6000)
